6c33449d6213bd5691d48e0fb37fa825e9a5519a
Vitor Pellegrino <vitorp@gmail.com>
2010-07-24 18:57:25 -0300

    Using the new rating as the landing page

--- config/routes.rb
+++ config/routes.rb
@@ -39,6 +39,7 @@ ActionController::Routing::Routes.draw do |map|
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
   map.resources :ratings
+  map.root :controller => "ratings" , :action => "new"
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
