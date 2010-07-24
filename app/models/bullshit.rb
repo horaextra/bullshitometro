4eceebf6166bcbd419d2623b86f5a1d50d809c02
Vitor Pellegrino <vitorp@gmail.com>
2010-07-24 18:15:16 -0300

    Using the yaml as the bullshits dictionary

--- app/models/bullshit.rb
+++ app/models/bullshit.rb
@@ -1,6 +1,6 @@
 class Bullshit
-  BULLSHIT_LIST = [ "sinergia"]
   def self.is_bullshit? word
-    BULLSHIT_LIST.include? word
+    @bullshits = YAML.load_file("#{RAILS_ROOT}/db/bullshits.yml")
+    @bullshits.include? word 
   end
 end 
--- spec/models/bullshit_spec.rb
+++ spec/models/bullshit_spec.rb
@@ -2,9 +2,17 @@ require "spec_helper"
 
 describe Bullshit do
   describe ".is_bullshit?" do
+    before :each do
+      @bullshits_list  = ["sinergia", "workflow"]
+      YAML.stub!(:load_file).with("#{RAILS_ROOT}/db/bullshits.yml").and_return(@bullshits_list)
+    end 
     it "should verify if the given word is on the bullshit list" do
       Bullshit.is_bullshit?("sinergia").should be_true
       Bullshit.is_bullshit?("unitario").should be_false
     end
+    it "should load the list of known bullshits" do
+      YAML.should_receive(:load_file).with("#{RAILS_ROOT}/db/bullshits.yml")
+      Bullshit.is_bullshit?("foo")
+    end 
   end
 end
