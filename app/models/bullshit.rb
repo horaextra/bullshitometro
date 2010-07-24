4502d5aa0ff4ca5d8bfdc265c44f787646365524
Vitor Pellegrino <vitorp@gmail.com>
2010-07-24 18:21:40 -0300

    Spliting the words

--- app/models/bullshit.rb
+++ app/models/bullshit.rb
@@ -1,6 +1,6 @@
 class Bullshit
   def self.is_bullshit? word
-    @bullshits = YAML.load_file("#{RAILS_ROOT}/db/bullshits.yml")
+    @bullshits = YAML.load_file("#{RAILS_ROOT}/db/bullshits.yml").split
     @bullshits.include? word 
   end
 end 
--- spec/models/bullshit_spec.rb
+++ spec/models/bullshit_spec.rb
@@ -3,8 +3,13 @@ require "spec_helper"
 describe Bullshit do
   describe ".is_bullshit?" do
     before :each do
-      @bullshits_list  = ["sinergia", "workflow"]
-      YAML.stub!(:load_file).with("#{RAILS_ROOT}/db/bullshits.yml").and_return(@bullshits_list)
+      @bullshits_string  = "sinergia workflow"
+      @bullshit_list = ['sinergia', 'workflow']
+      YAML.stub!(:load_file).with("#{RAILS_ROOT}/db/bullshits.yml").and_return(@bullshits_string)
+    end 
+    it "should split the string found at the bullshit's yaml dictionary" do
+      @bullshits_string.should_receive(:split).and_return(@bullshit_list)
+      Bullshit.is_bullshit?("sinergia")
     end 
     it "should verify if the given word is on the bullshit list" do
       Bullshit.is_bullshit?("sinergia").should be_true
