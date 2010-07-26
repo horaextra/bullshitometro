a00397b45747e792d7fbc0ecc3b3a2d3e11e76dc
Vitor Pellegrino <vitorp@gmail.com>
2010-07-26 01:12:01 -0300

    Refactoring some texts for legibility

--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -17,11 +17,21 @@ describe Rating do
       Rating.rate("foo").should == 1
     end
 
-    it "should return the number of bullshits found" do
-      Bullshit.stub!("is_bullshit?").with("foo").and_return(true)
-      Bullshit.stub!("is_bullshit?").with("bar").and_return(false)
-      Rating.rate("foo bar").should == 1
-    end
-
+    context "when counting bullshits in a text" do
+      before do
+        Bullshit.stub!("is_bullshit?").with("foo").and_return(true)
+        Bullshit.stub!("is_bullshit?").with("bar").and_return(false)
+      end 
+      
+      it "should return 1 for a bullshit found" do
+        Rating.rate("foo").should == 1 
+      end 
+      it "should return 0 for no bullshit" do
+        Rating.rate("bar").should == 0
+      end   
+      it "should return the number of bullshits found in a given text"  do
+        Rating.rate("foo bar").should == 1
+      end 
+    end 
   end 
 end 
